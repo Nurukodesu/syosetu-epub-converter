261756a93157841b7da3b70129bd1bfc77a9844a
Nurukodesu <ath455756@gmail.com>
2024-07-13 17:50:42 +0630

    made it work with 2024 syosetu

--- yomou2ebook.sh
+++ yomou2ebook.sh
@@ -7,14 +7,14 @@ TMP_DIR=$SELF_DIR/tmp
 
 usage () {
     echo "usage:" 
-    echo "  $0 <main_url>"
+    echo "  $0 <main_url> <from_nth_chapter> <to_nth_chapter>"
     echo ""
     echo "where: "
     echo "  <main_url> is the url of the main page of the book"
     echo "             for example: http://ncode.syosetu.com/n6316bn"
 }
 
-if [[ $# != 1 ]]; then
+if [[ $# != 3 ]]; then
     usage
     exit 1
 fi
@@ -24,6 +24,8 @@ MAIN_URL="$1"
 MAIN_URL=${1%/}
 TXT_OUT="output.txt"
 EPUB_OUT="output.epub"
+START=$2
+END=$3
 
 # IP will be banned for a short time if pages are loaded too fast
 SLEEP_TIME=0.1
@@ -36,7 +38,7 @@ mkdir $TMP_DIR
 pushd $TMP_DIR > /dev/null
 
 # Load main page
-wget -q $MAIN_URL -O main_page.html
+wget -q --user-agent="Mozilla" $MAIN_URL -O main_page.html
 sleep $SLEEP_TIME
 # <p class="nover_title">XXX</p>
 NOVEL_TITLE=`pcregrep -Mo '(?s)<p class=\"novel_title\">.*?</p>' main_page.html | sed -E 's/<[^>]*>//g'`
@@ -44,15 +46,12 @@ NOVEL_TITLE=`pcregrep -Mo '(?s)<p class=\"novel_title\">.*?</p>' main_page.html 
 WRITER_NAME=`pcregrep -Mo '(?s)<div class=\"novel_writername\">.*?</div>' main_page.html | grep -Po '<a .*?>.*?</a>' | sed -E 's/<[^>]*>//g'`
 # <div id="novel_ex">XXX</div>
 NOVEL_SUMMARY=`pcregrep -Mo '(?s)<div id=\"novel_ex\">.*?</div>' main_page.html | sed -E 's/<[^>]*>//g'`
-# <dd class="subtitle"><a href="XXX">...</a></dd>
-CHAPTER_LIST=`pcregrep -Mo '(?s)<dd class=\"subtitle\">.*?</dd>' main_page.html | grep -Po 'href=\".*?\"' | sed -E 's/^.*href=\"([^"])\".*$/\1/g' | cut -d'/' -f3`
 
 echo "Found the following novel:"
 echo "Title: $NOVEL_TITLE"
 echo "Author: $WRITER_NAME"
 echo "Summary:"
 echo "$NOVEL_SUMMARY"
-echo "Number of chapters: `echo $CHAPTER_LIST | wc -w`"
 echo ""
 
 if [[ -e $TXT_OUT ]]; then
@@ -67,8 +66,8 @@ echo "$NOVEL_SUMMARY" >> $TXT_OUT
 echo "" >> $TXT_OUT
 echo "" >> $TXT_OUT
 
-for chapter_page in $CHAPTER_LIST; do
-    wget -q $MAIN_URL/$chapter_page -O $chapter_page.html
+for chapter_page in $(seq $START $END); do
+    wget --user-agent="Mozilla" -q $MAIN_URL/$chapter_page -O $chapter_page.html
     sleep $SLEEP_TIME
     # <p class="novel_subtitle">XXX</p>
     chapter_title=`pcregrep -Mo '(?s)<p class=\"novel_subtitle\">.*?</p>' $chapter_page.html | sed -E 's/<[^>]*>//g'`
@@ -85,7 +84,7 @@ for chapter_page in $CHAPTER_LIST; do
             line_id=`echo $paragraph | sed -E 's/^.*<p id=\"([^"]*)\">.*$/\1/g'`
             image_path=`echo $image | sed -E 's/^.*src=\"([^"]*)\".*$/\1/g'`
             image_name=`echo $image | sed -E 's/^.*alt=\"([^"]*)\".*$/\1/g'`
-            wget -q http:$image_path -O ${chapter_page}_${line_id}.jpg
+            wget -q --user-agent="Mozilla" http:$image_path -O ${chapter_page}_${line_id}.jpg
             sleep $SLEEP_TIME
             paragraph=`echo $paragraph | sed -E 's/<img [^>]*>/!['"$image_name"']('"${chapter_page}_${line_id}.jpg"')/g'`
         fi
