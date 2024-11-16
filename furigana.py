c6afa8c929298e4222b6919547ed51d3cc3f887d
Nurukodesu <ath455756@gmail.com>
2024-11-16 14:54:43 +0630

    added actual furigana support

---
+++ furigana.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from pandocfilters import toJSONFilter, RawInline
+
+
+def behead(key, value, format, meta):
+	if format == 'epub' and key == 'Link' and value[2][0][0] == '-':
+		return RawInline('html',"<ruby>"+ value[1][0]['c'] +"<rp>(</rp><rt>"+value[2][0][1:]+"</rt><rp>)</rp></ruby>")
+
+if __name__ == "__main__":
+	toJSONFilter(behead)
