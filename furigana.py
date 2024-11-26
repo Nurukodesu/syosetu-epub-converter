7b280a275d5077cae59bc3f8b45e29c3b47cf59f
Nurukodesu <ath455756@gmail.com>
2024-11-26 17:10:15 +0630

    Fixed furigana not working if there is quotation

--- furigana.py
+++ furigana.py
@@ -5,6 +5,8 @@ from pandocfilters import toJSONFilter, RawInline
 
 def behead(key, value, format, meta):
 	if format == 'epub' and key == 'Link' and value[2][0][0] == '-':
+		if value[1][0]['t'] == 'Quoted':
+			return RawInline('html', "\"<ruby>"+ value[1][0]['c'][1][0]['c'] + "<rp>(</rp><rt>"+value[2][0][1:]+"</rt><rp>)</rp></ruby>\"")
 		return RawInline('html',"<ruby>"+ value[1][0]['c'] +"<rp>(</rp><rt>"+value[2][0][1:]+"</rt><rp>)</rp></ruby>")
 
 if __name__ == "__main__":
